86d14e0770a15dedd31dcf3f7670b766329f9a0f
CrossNox <ijmermet@gmail.com>
2022-04-18 05:16:47 -0300

    query interface

--- metrics_server/client/cli.py
+++ metrics_server/client/cli.py
@@ -1,7 +1,11 @@
+from typing import Optional
+from datetime import datetime
+
 import typer
 
 from metrics_server.utils import get_logger
 from metrics_server.client.client import Client
+from metrics_server.constants import Aggregation
 from metrics_server.protocol import Metric, MetricResponse
 
 logger = get_logger(__name__)
@@ -22,6 +26,18 @@ def send(metric: str, value: int, host: str = "localhost", port: int = 5678):
         logger.error("Got error: %s", status.msg)
     else:
         logger.info("Message received correctly!")
+
+
+@app.command()
+def query(
+    metric: str,
+    agg: Aggregation,
+    agg_window: float,
+    start: Optional[datetime] = None,
+    end: Optional[datetime] = None,
+):
+    assert agg_window > 0
+    logger.info("%s %s %s %s %s", metric, start, end, agg, agg_window)
 
 
 if __name__ == "__main__":
---
+++ metrics_server/constants.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+
+class Aggregation(Enum):
+    AVG = "avg"
+    MIN = "min"
+    MAX = "max"
+    COUNT = "cnt"
